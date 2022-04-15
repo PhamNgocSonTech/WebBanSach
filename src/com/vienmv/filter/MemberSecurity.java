89217b427fbd5efcd71638732a61f1d080c01355
PhamNgocSon <phamngocson7a1@gmail.com>
2022-04-15 14:36:48 +0700

    FixFullBugProject

--- src/com/vienmv/filter/MemberSecurity.java
+++ src/com/vienmv/filter/MemberSecurity.java
@@ -9,10 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 
 import com.vienmv.model.User;
 @WebFilter(urlPatterns="/member/*")
