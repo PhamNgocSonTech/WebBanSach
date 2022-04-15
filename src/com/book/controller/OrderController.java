f9e2b4b60c28d21329dfff43b77087837b9f10a2
PhamNgocSon <phamngocson7a1@gmail.com>
2022-04-15 20:43:01 +0700

    Rename Project + Package

---
+++ src/com/book/controller/OrderController.java
@@ -0,0 +1,72 @@
+package com.book.controller;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.book.model.Cart;
+import com.book.model.CartItem;
+import com.book.model.User;
+import com.book.service.CartItemService;
+import com.book.service.CartService;
+import com.book.service.UserService;
+import com.book.service.impl.CartServiceImpl;
+import com.book.service.impl.CartServiceItemImpl;
+import com.book.service.impl.UserServiceImpl;
+import com.book.tools.SendMail;
+import com.book.util.RandomUUID;
+
+@WebServlet(urlPatterns = "/member/order")
+public class OrderController extends HttpServlet {
+	UserService userService = new UserServiceImpl();
+	CartService cartService = new CartServiceImpl();
+	CartItemService cartItemService = new CartServiceItemImpl();
+	long time = System.currentTimeMillis();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		Object obj = session.getAttribute("account");
+		User buyer = (User) obj;
+		Cart cart = new Cart();
+		cart.setBuyer(buyer);
+		cart.setBuyDate(new java.sql.Date(time));
+		cart.setId(RandomUUID.getRandomID());
+		cartService.insert(cart);
+
+		Object objCart = session.getAttribute("cart");
+		if (objCart != null) {
+			// ep ve dung kieu cua no khi them vao o phan them vao gio hang controller
+			Map<Integer, CartItem> map = (Map<Integer, CartItem>) objCart;
+
+			for (CartItem cartItem : map.values()) {
+				cartItem.setCart(cart);
+				cartItem.setId(RandomUUID.getRandomID());
+				SendMail sm = new SendMail();
+				sm.sendMail(cart.getBuyer().getEmail(), "UNIFY", "Payment success. We will contact you soon ! ");
+				cartItemService.insert(cartItem);
+				
+			}
+
+		}
+		session.removeAttribute("cart");
+		resp.sendRedirect(req.getContextPath() + "/home");
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
---
+++ src/com/book/jdbc/JDBCConnection.java
@@ -0,0 +1,25 @@
+package com.book.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+	public static Connection getJDBCConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String user = "sa";
+			String pass = "sa123";
+			String url = "jdbc:sqlserver://localhost:1433;databaseName=UNIFY";
+			connection = DriverManager.getConnection(url, user, pass);
+			System.out.println("thanh cong");
+			
+		} catch (ClassNotFoundException | SQLException ex) {
+			ex.printStackTrace();
+		}
+
+		return connection;
+	}
+	
+}
